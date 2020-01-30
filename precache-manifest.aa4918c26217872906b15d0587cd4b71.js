6cb06d7255de94ebc38c3b33d8d128a4d614bc8c
anett1 <anett@poczta.fm>
2020-01-30 15:30:08 -0500

    Updates

---
+++ precache-manifest.aa4918c26217872906b15d0587cd4b71.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "378a32e32e372554ef3a61939b8c23b1",
+    "url": "/currencyConverter-React/index.html"
+  },
+  {
+    "revision": "d1dd688674c55a3c5e65",
+    "url": "/currencyConverter-React/static/css/main.94b3ceff.chunk.css"
+  },
+  {
+    "revision": "3779444c6ab72e998a57",
+    "url": "/currencyConverter-React/static/js/2.306fe892.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/currencyConverter-React/static/js/2.306fe892.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d1dd688674c55a3c5e65",
+    "url": "/currencyConverter-React/static/js/main.90daf89e.chunk.js"
+  },
+  {
+    "revision": "342dc6b7c836f5223fbc",
+    "url": "/currencyConverter-React/static/js/runtime-main.48ac1fd1.js"
+  }
+]);
